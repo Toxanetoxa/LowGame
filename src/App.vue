9a74c1b3b6b1fb83c40ef5c7b0739f0dd07aa2c0
asotnik <toxanetoxa@gmail.com>
2022-06-27 04:20:02 +0300

    first Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="app">
+    <section class="container">
+      <div class="container-head"></div>
+      <div class="container-body">
+        <game-frame/>
+      </div>
+      <div class="container-footer">
+        <g-button :value="'reset'"/>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import GameFrame from "@/components/GameFrame/gameFrame";
+import GButton from "@/components/ui/g-button";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GButton,
+    GameFrame
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "./assets/css/app";
+
+.app {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background: $color_main_white;
+
+  .container {
+    position: relative;
+    width: 80%;
+    margin: 0 auto;
+    height: 80%;
+    background: $color_bg_game_frame;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    &-head,
+    &-footer {
+      height: 10%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    &-body {
+      height: calc(80% - 20px);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+
+    &-head,
+    &-body,
+    &-footer {
+      border-bottom: 1px solid $color-main_white;
+    }
+  }
 }
 </style>
